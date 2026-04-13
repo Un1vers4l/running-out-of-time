d1ed7795e6f60a3a3145331d87e11859117952bd
Fabian Heeke <Fa.Heeke@googlemail.com>
2026-04-13 11:35:32 +0200

    Added color highlight for closest object

--- Assets/_Game/Scripts/EnvironmentInteractions/InteractableBase.cs
+++ Assets/_Game/Scripts/EnvironmentInteractions/InteractableBase.cs
@@ -9,10 +9,28 @@ public abstract class InteractableBase : MonoBehaviour, IInteractable
     PlayInteractionSound();
   }
 
+  private static readonly Color _highlightColor = new Color32(173, 216, 200, 255);
+  private readonly System.Collections.Generic.Dictionary<SpriteRenderer, Color> _defaultSpriteColors
+    = new System.Collections.Generic.Dictionary<SpriteRenderer, Color>();
+
+  // TODO: Maybe do this differently; quick llm solution for until we know for sure which sprite we use
   public virtual void SetHighlightInUI(bool isHighlighted)
   {
-    // default highlight logic
-    Debug.Log("Highlight object: " + gameObject.name);
+    var spriteRenderers = GetComponentsInChildren<SpriteRenderer>(true);
+
+    foreach (var spriteRenderer in spriteRenderers)
+    {
+      if (spriteRenderer == null) continue;
+
+      if (!_defaultSpriteColors.ContainsKey(spriteRenderer))
+      {
+        _defaultSpriteColors[spriteRenderer] = spriteRenderer.color;
+      }
+
+      spriteRenderer.color = isHighlighted
+        ? _highlightColor
+        : _defaultSpriteColors[spriteRenderer];
+    }
   }
 
   protected abstract void ExecuteInteraction(GameObject interactionSource);
