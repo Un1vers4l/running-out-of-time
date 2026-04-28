ce711a87d4048d700669b9274590d7c9054c2dde
Fabian Heeke <Fa.Heeke@googlemail.com>
2026-04-28 17:25:10 +0200

    Added possiblity for npc cultists to lie down

--- Assets/_Game/Scripts/EnvironmentInteractions/KultistNPC.cs
+++ Assets/_Game/Scripts/EnvironmentInteractions/KultistNPC.cs
@@ -1,14 +1,22 @@
 using UnityEngine;
+[RequireComponent(typeof(SpriteRenderer))]
 
+[RequireComponent(typeof(Animator))]
 public class KultistNPC : InteractableBase
 {
   [SerializeField] private NPC_Data npcData;
 
   private SpriteRenderer _spriteRenderer;
+  private Animator _animator;
+  private readonly string _lyingDownAnimationTriggerName = "isLyingDown";
+
+  private bool _isLyingDown = false;
 
   void Awake()
   {
     _spriteRenderer = GetComponent<SpriteRenderer>();
+    _animator = GetComponent<Animator>();
+
     _spriteRenderer.color = npcData.robeColor;
   }
 
@@ -17,6 +25,12 @@ public class KultistNPC : InteractableBase
     if (!npcData) return;
 
     DialogueManager.Instance.StartDialogue(npcData.displayName, npcData.dialogJSON);
+  }
+
+  private void SetIsLyingDown(bool isLyingDown)
+  {
+    _isLyingDown = isLyingDown;
+    _animator.SetBool(_lyingDownAnimationTriggerName, isLyingDown);
   }
 
   protected override void PlayInteractionSound()
