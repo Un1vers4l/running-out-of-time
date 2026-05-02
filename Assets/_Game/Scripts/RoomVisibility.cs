bf95c348e5606b8eb5b07271a649fcf455805da7
Joana Wegener <joana.wegener98@gmail.com>
2026-05-02 10:37:02 +0200

    add fade in to showing areas

--- Assets/_Game/Scripts/RoomVisibility.cs
+++ Assets/_Game/Scripts/RoomVisibility.cs
@@ -1,9 +1,11 @@
+using System.Collections;
 using UnityEngine;
 
 [RequireComponent(typeof(Collider2D))]
 public class RoomVisibility : MonoBehaviour
 {
     [SerializeField] private SpriteRenderer fogOverlay;
+    [SerializeField] private float fadeDuration = 1f;
 
     private bool _discovered;
 
@@ -13,6 +15,22 @@ public class RoomVisibility : MonoBehaviour
         if (!collision.TryGetComponent<PlayerMovement>(out _)) return;
 
         _discovered = true;
+        StartCoroutine(FadeOut());
+    }
+
+    private IEnumerator FadeOut()
+    {
+        Color color = fogOverlay.color;
+        float elapsed = 0f;
+
+        while (elapsed < fadeDuration)
+        {
+            elapsed += Time.deltaTime;
+            color.a = Mathf.Lerp(1f, 0f, elapsed / fadeDuration);
+            fogOverlay.color = color;
+            yield return null;
+        }
+
         fogOverlay.enabled = false;
     }
 }
